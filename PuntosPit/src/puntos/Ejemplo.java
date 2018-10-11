275de8b1213ee1131d56e88da2e715a4c85eba26
SUITE <SUITE@SUITEE102-ST20.cibertec.edu.pe>
2018-10-10 21:08:05 -0500

    Hola

---
+++ PuntosPit/src/puntos/Ejemplo.java
@@ -0,0 +1,16 @@
+package puntos;
+
+public class Ejemplo {
+	public static void main(String[] args) {
+		System.err.print("Hola Serrano");
+	}
+	public void saludo() {
+		System.err.print("Hola Serrano");
+	}
+	public void saludo2() {
+		System.err.print("Hola Serrano");
+	}
+	private String modo() {
+		return "";
+	}
+}
